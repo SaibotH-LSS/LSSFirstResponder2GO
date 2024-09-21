54d1de5cb24a32c3d5e6c634a632b6364b37d4d9
SaibotH <162214755+SaibotH-LSS@users.noreply.github.com>
2024-09-21 16:05:06 +0200

    Update LSSFirstResponder2GO.user.js

--- LSSFirstResponder2GO.user.js
+++ LSSFirstResponder2GO.user.js
@@ -1,1 +1,19 @@
+// ==UserScript==
+// @name         [LSS]FirstResponderReloaded
+// @namespace    FirstRespond
+// @version      3.0.0
+// @description  Wählt das nächstgelegene FirstResponder-Fahrzeug aus (Original von JuMaHo und DrTraxx)
+// @author       SaibotH
+// @license      MIT
+// @homepage     https://github.com/SaibotH-LSS/LSSFirstResponder2GO
+// @homepageURL  https://github.com/SaibotH-LSS/LSSFirstResponder2GO
+// @supportURL   https://github.com/SaibotH-LSS/LSSFirstResponder2GO/issues
+// @updateURL    https://raw.githubusercontent.com/SaibotH-LSS/LSSFirstResponder2GO/refs/heads/main/LSSFirstResponder2GO.user.js
+// @downloadURL  https://raw.githubusercontent.com/SaibotH-LSS/LSSFirstResponder2GO/refs/heads/main/LSSFirstResponder2GO.user.js
+// @icon         https://www.leitstellenspiel.de/favicon.ico
+// @match        *.leitstellenspiel.de/aaos/*/edit
+// @run-at       document-idle
+// ==/UserScript==
 
+// Definition von globalen Variablen um Fehlermeldungen zu unterdrücken
+/* global $,I18n */
